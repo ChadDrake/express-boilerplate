2dd6127c6e4dfe0b7441ab3b527ad0c22353f96a
Chad Drake <chaddrake1313@yahoo.com>
2020-09-17 14:35:24 -0500

    fixed error handling, added json parsing

--- src/app.js
+++ src/app.js
@@ -9,12 +9,11 @@ const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
+app.use(express.json());
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-app.use(errorHandler());
-
-function errorHandler(error, req, res, next) {
+app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
@@ -23,7 +22,7 @@ function errorHandler(error, req, res, next) {
     response = { message: error.message, error };
   }
   res.status(500).json(response);
-}
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
